0e89cf6b4713ab54199bf3c2c4e30dd3a395c7fa
Parth <pparekh719@gmail.com>
2021-04-11 14:00:49 +0530

    Bottom overlap issue solved

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:toast/toast.dart';
 
 void main() {
   runApp(MyApp());
@@ -44,196 +45,206 @@ class _MyAppState extends State<MyApp> {
             children: [
               Expanded(
                 flex: 9,
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                      bottomRight: Radius.circular(30),
-                      bottomLeft: Radius.circular(30),
-                    ),
-                    color: Colors.white,
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.asset(
-                        'assets/dh.png',
-                        height: 120,
-                        width: 120,
+                child: SingleChildScrollView(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        bottomRight: Radius.circular(30),
+                        bottomLeft: Radius.circular(30),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 0, horizontal: 0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              'Doc ',
-                              style: TextStyle(
-                                fontSize: 30,
-                                fontFamily: 'Pacifico',
-                                color: Color(0xFF0562A7),
+                      color: Colors.white,
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Image.asset(
+                            'assets/dh.png',
+                            height: 120,
+                            width: 120,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 0, horizontal: 0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'Doc ',
+                                style: TextStyle(
+                                  fontSize: 30,
+                                  fontFamily: 'Pacifico',
+                                  color: Color(0xFF0562A7),
+                                ),
                               ),
-                            ),
-                            Text(
-                              'Hub',
+                              Text(
+                                'Hub',
+                                style: TextStyle(
+                                  fontSize: 30,
+                                  fontFamily: 'Pacifico',
+                                  color: Color(0xFF37DBFF),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 0, horizontal: 20),
+                          child: Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              'Login',
                               style: TextStyle(
-                                fontSize: 30,
-                                fontFamily: 'Pacifico',
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold,
                                 color: Color(0xFF37DBFF),
                               ),
-                            )
-                          ],
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 0, horizontal: 20),
-                        child: Align(
-                          alignment: Alignment.centerLeft,
-                          child: Text(
-                            'Login',
-                            style: TextStyle(
-                              fontSize: 25,
-                              fontWeight: FontWeight.bold,
-                              color: Color(0xFF37DBFF),
                             ),
                           ),
                         ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 0, horizontal: 20),
-                        child: Align(
-                          alignment: Alignment.centerLeft,
-                          child: Text(
-                            'To continue with your account',
-                            style: TextStyle(
-                              fontSize: 18,
-                              color: Color(0xFF37DBFF),
-                            ),
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(20, 20, 20, 5),
-                        child: TextField(
-                          onChanged: (value) {
-                            username = value;
-                          },
-                          style: TextStyle(color: Color(0xFF0562A7)),
-                          decoration: InputDecoration(
-                            labelText: 'Username',
-                            hintText: 'Enter username',
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Color(0xFF37DBFF)),
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Color(0xFF0562A7)),
-                            ),
-                            border: const OutlineInputBorder(),
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(20, 20, 20, 5),
-                        child: TextField(
-                          onChanged: (value) {
-                            password = value;
-                          },
-                          obscureText: isObscure,
-                          style: TextStyle(color: Color(0xFF0562A7)),
-                          decoration: InputDecoration(
-                            suffixIcon: IconButton(
-                              icon: Icon(
-                                isObscure
-                                    ? Icons.visibility
-                                    : Icons.visibility_off,
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 0, horizontal: 20),
+                          child: Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              'To continue with your account',
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: Color(0xFF37DBFF),
                               ),
-                              onPressed: () {
-                                setState(() {
-                                  isObscure = !isObscure;
-                                });
-                              },
-                            ),
-                            labelText: 'Password',
-                            hintText: 'Enter Password',
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Color(0xFF37DBFF)),
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Color(0xFF0562A7)),
-                            ),
-                            border: const OutlineInputBorder(),
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 0, horizontal: 20),
-                        child: Align(
-                          alignment: Alignment.centerRight,
-                          child: Text(
-                            '?Forgot Password',
-                            textDirection: TextDirection.rtl,
-                            style: TextStyle(
-                              color: Color(0xFF37DBFF),
                             ),
                           ),
                         ),
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      GestureDetector(
-                        onTap: () async {
-                          showLoader();
-                          // print("Username ->$username\nPassword ->$password");
-                          var url = Uri.parse(
-                              'https://dattarubberindustries.com/api/login.php');
-                          var response = await http.post(url, body: {
-                            'usr_email': '$username',
-                            'usr_pass': '$password'
-                          });
-                          Future.delayed(Duration(milliseconds: 3000), () {
-                            hideLoader();
-                          });
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(20, 20, 20, 5),
+                          child: TextField(
+                            onChanged: (value) {
+                              username = value;
+                            },
+                            style: TextStyle(color: Color(0xFF0562A7)),
+                            decoration: InputDecoration(
+                              labelText: 'Username',
+                              hintText: 'Enter username',
+                              enabledBorder: OutlineInputBorder(
+                                borderSide:
+                                    BorderSide(color: Color(0xFF37DBFF)),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide:
+                                    BorderSide(color: Color(0xFF0562A7)),
+                              ),
+                              border: const OutlineInputBorder(),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(20, 20, 20, 5),
+                          child: TextField(
+                            onChanged: (value) {
+                              password = value;
+                            },
+                            obscureText: isObscure,
+                            style: TextStyle(color: Color(0xFF0562A7)),
+                            decoration: InputDecoration(
+                              suffixIcon: IconButton(
+                                icon: Icon(
+                                  isObscure
+                                      ? Icons.visibility
+                                      : Icons.visibility_off,
+                                ),
+                                onPressed: () {
+                                  setState(() {
+                                    isObscure = !isObscure;
+                                  });
+                                },
+                              ),
+                              labelText: 'Password',
+                              hintText: 'Enter Password',
+                              enabledBorder: OutlineInputBorder(
+                                borderSide:
+                                    BorderSide(color: Color(0xFF37DBFF)),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide:
+                                    BorderSide(color: Color(0xFF0562A7)),
+                              ),
+                              border: const OutlineInputBorder(),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 0, horizontal: 20),
+                          child: Align(
+                            alignment: Alignment.centerRight,
+                            child: Text(
+                              '?Forgot Password',
+                              textDirection: TextDirection.rtl,
+                              style: TextStyle(
+                                color: Color(0xFF37DBFF),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        GestureDetector(
+                          onTap: () async {
+                            showLoader();
+                            // print("Username ->$username\nPassword ->$password");
+                            var url = Uri.parse(
+                                'https://dattarubberindustries.com/api/login.php');
+                            var response = await http.post(url, body: {
+                              'usr_email': '$username',
+                              'usr_pass': '$password'
+                            });
+                            await Future.delayed(Duration(milliseconds: 3000),
+                                () {
+                              hideLoader();
+                            });
 
-                          if (jsonDecode(response.body)['success'] == 1) {
-                            print("Success");
-                          } else {
-                            print("Failure");
-                          }
-                        },
-                        child: Container(
-                          width: 250,
-                          height: 50,
-                          decoration: BoxDecoration(
-                            color: Color(0xFF0562A7),
-                            borderRadius: BorderRadius.circular(30),
-                          ),
-                          child: loginChild,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Container(
-                        width: 250,
-                        height: 50,
-                        decoration: BoxDecoration(
-                          color: Color(0xFF37DBFF),
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                        child: Center(
-                          child: Text(
-                            'Request Access',
-                            style: TextStyle(
-                              color: Colors.white,
+                            if (jsonDecode(response.body)['success'] == 1) {
+                              print("Success");
+                            } else {
+                              print("Failure");
+                            }
+                          },
+                          child: Container(
+                            width: 250,
+                            height: 50,
+                            decoration: BoxDecoration(
+                              color: Color(0xFF0562A7),
+                              borderRadius: BorderRadius.circular(30),
                             ),
+                            child: loginChild,
                           ),
                         ),
-                      ),
-                    ],
+                        SizedBox(
+                          height: 40,
+                        ),
+                        // Container(
+                        //   width: 250,
+                        //   height: 50,
+                        //   decoration: BoxDecoration(
+                        //     color: Color(0xFF37DBFF),
+                        //     borderRadius: BorderRadius.circular(30),
+                        //   ),
+                        //   child: Center(
+                        //     child: Text(
+                        //       'Request Access',
+                        //       style: TextStyle(
+                        //         color: Colors.white,
+                        //       ),
+                        //     ),
+                        //   ),
+                        // ),
+                      ],
+                    ),
                   ),
                 ),
               ),
